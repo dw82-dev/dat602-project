972117d5d13c3de341381e1fd2c9484a50b8aad3
dw <55720979+dw82-dev@users.noreply.github.com>
2021-06-14 09:55:49 +1200

    added dataaccess classes

---
+++ 003 - Milestone Three/WizardQuestGUIApp/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardQuestConsoleApp
+{
+    class Inventory
+    {
+        public static Inventory CurrentInventory { get; set; }
+        public string Item;
+        public int Quantity;
+    }
+}
