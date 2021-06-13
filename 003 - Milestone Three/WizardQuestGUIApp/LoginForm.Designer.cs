418b87555ef6632fb75b26277995fe72441681ee
dw <55720979+dw82-dev@users.noreply.github.com>
2021-06-14 09:46:17 +1200

    added intial GUI app project files and classes

---
+++ 003 - Milestone Three/WizardQuestGUIApp/LoginForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace WizardQuestGUIApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.wizardPicture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(39, 332);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(101, 23);
+            this.deleteButton.TabIndex = 0;
+            this.deleteButton.Text = "Delete Account";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(170, 332);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // wizardPicture
+            // 
+            this.wizardPicture.Image = ((System.Drawing.Image)(resources.GetObject("wizardPicture.Image")));
+            this.wizardPicture.Location = new System.Drawing.Point(52, 59);
+            this.wizardPicture.Name = "wizardPicture";
+            this.wizardPicture.Size = new System.Drawing.Size(179, 154);
+            this.wizardPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.wizardPicture.TabIndex = 2;
+            this.wizardPicture.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Wizard Quest";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(39, 252);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(206, 20);
+            this.username.TabIndex = 4;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(39, 285);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(206, 20);
+            this.password.TabIndex = 5;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 384);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wizardPicture);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.deleteButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.wizardPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.PictureBox wizardPicture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+    }
+}
+
